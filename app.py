734411da361d5aa03b2533f298c9eabfe9e1c06e
Peace-M <gladys.mwatati@strathmore.edu>
2021-05-29 20:15:01 +0300

    created several files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  
+
+@app.route('/portfolio-page.html')
+def portfolio():
+    return render_template('portfolio-page.html')  
+
+@app.route('/portfolioII-page.html')
+def portfolioll():
+    return render_template('portfolioII-page.html')
